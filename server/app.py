747f94bda943542e2fb69c39e6297a21fd477b48
RayRae511 <oetaray@gmail.com>
2023-09-28 13:21:00 +0300

    completed wrk

--- server/app.py
+++ server/app.py
@@ -20,19 +20,24 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return jsonify(Bakery.serialize_list(bakeries))
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    if bakery:
+        return jsonify(Bakery.serialize())
+    return jsonify({'error': 'Bakery not found'}), 404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify(BakedGood.serialize_list(baked_goods))
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify(BakedGood.serialize(baked_good[0]))
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, String, Integer, Float, ForeignKey
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -12,9 +12,16 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
-
+    name = db.Column(db.String(255))
+    created = db.Column(String(255))
+    updated = db.Column(String(255))
+    baked_goods = db.relationship('BakedGood', backref='bakery')
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    created = db.Column(String(255))
+    updated = db.Column(String(255))
+    bakery_id = db.Column(db.Integer, ForeignKey('bakeries.id'))
